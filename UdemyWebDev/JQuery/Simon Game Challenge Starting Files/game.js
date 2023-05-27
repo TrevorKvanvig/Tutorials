9259ca5910b96cb5fbb90ebd9d8c2e5939b4b55c
TrevorKvanvig <tkvanvig10@gmail.com>
2023-05-27 13:33:24 -0700

    finished simon game

--- UdemyWebDev/JQuery/Simon Game Challenge Starting Files/game.js
+++ UdemyWebDev/JQuery/Simon Game Challenge Starting Files/game.js
@@ -4,33 +4,46 @@ let userPattern = []
 let randomColor = ""
 let started = false
 let level = 0
+let userPatternIndex = 0
 
-function nextSequnce(){
-    min = Math.ceil(0);
-    max = Math.floor(4);
-    randomNum = Math.floor(Math.random() * (max - min) + min)
-    randomColor = buttonColors[randomNum]
-    gamePattern.push(randomColor)
-    $("h1").text("LEVEL " + level )
-    level++
-    return buttonColors[randomNum]
+function startOver(){
+    gamePattern = []
+    userPattern = []
+    randomColor = ""
+    started = false
+    level = 0
+    userPatternIndex = 0
+    setTimeout(function(){
+       $("h1").text("Press A Key to Start") 
+    }, 1500)
+    
 }
 
-$(document).on("keypress", function(e){
-    if (started == false){
-        nextSequnce()
-        buttonClick(randomColor) 
-    }
-    started = true
-})
+function gameOver(){
+    $("h1").text("GAME OVER")
+    audio = new Audio("sounds/wrong.mp3")
+    audio.play()
+    $("body").toggleClass("game-over")
+    setTimeout(function(){
+        $("body").toggleClass("game-over")
+    }, 200)
+    startOver()
+}
+
 
 $(".btn").on("click", function(e){
-    console.log("clicked");
     userColor = this.id
     userPattern.push(userColor)
     buttonClick(userColor)
-    console.log(userPattern);
+    console.log("User Pattern =>", userPattern)
+    console.log("Game Pattern =>", gamePattern)
+    checkInput()
+    if(gamePattern[userPatternIndex] != userPattern[userPatternIndex]){
+        gameOver()
+    }
+    userPatternIndex++
 })
+
 
 function buttonClick(color){
     $(`.${color}`).toggleClass("pressed")
@@ -58,8 +71,63 @@ function buttonClick(color){
         default:
             break;
     }
+    
 }
 
-function animatePress(color){
+function generateColor(){
+    // generate random number
+    min = Math.ceil(0);
+    max = Math.floor(4);
+    randomNum = Math.floor(Math.random() * (max - min) + min)
 
+    // change random number to color
+    randomColor = buttonColors[randomNum]
+}
+
+function nextSequnce(){
+    if(started == true){
+       generateColor()
+        gamePattern.push(randomColor)
+    
+        buttonClick(randomColor);
+        // update level
+        $("h1").text("LEVEL " + level )
+        level++
+
+        console.log("User Pattern =>", userPattern)
+        console.log("Game Pattern =>", gamePattern) 
+    }
+    
+}
+
+
+$(document).on("keypress", function(e){
+
+    if (started == false){
+        started = true
+        nextSequnce()
+        
+    }
+    
+})
+
+function checkInput(){
+    if (started == true){
+        console.log(gamePattern.length);
+        console.log(gamePattern.length);
+
+        if(userPattern === gamePattern){
+            console.log("same")
+        }
+        if(gamePattern.length == userPattern.length){
+            
+            setTimeout(function(){
+                
+                userPattern = []
+                userPatternIndex = 0
+                nextSequnce()
+                
+            }, 1500)
+        }
+    }
 }
