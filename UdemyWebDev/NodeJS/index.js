132c741334ae2186fb93dbd0c7c43ec15802dd6c
TrevorKvanvig <tkvanvig10@gmail.com>
2023-05-29 15:42:17 -0700

    NPM Practice

--- UdemyWebDev/NodeJS/index.js
+++ UdemyWebDev/NodeJS/index.js
@@ -1,1 +1,10 @@
-const fs = require('fs');
+var superheroes = require("superheroes")
+const supervillains = require('supervillains');
+
+
+
+var villanName  = supervillains.random();
+var name = superheroes.random()
+
+console.log("superhero name:",name);
+console.log("supervillain name:",villanName);
