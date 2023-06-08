f60409e812038e1f518c34e3fca3f49a749be79e
TrevorKvanvig <tkvanvig10@gmail.com>
2023-06-07 20:21:04 -0700

    start MongoDB app

---
+++ UdemyWebDev/mongoDB_Start/app.js
@@ -0,0 +1,20 @@
+const secrets = require(__dirname + "/secrets.js")
+
+const { MongoClient } = require("mongodb");
+// Replace the uri string with your connection string.
+const uri = "mongodb+srv://Cluster91362:"+secrets.getPassword()+"@cluster91362.pghfyon.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri);
+async function run() {
+  try {
+    const database = client.db('sample_mflix');
+    const movies = database.collection('movies');
+    // Query for a movie that has the title 'Back to the Future'
+    const query = { title: 'Back to the Future' };
+    const movie = await movies.findOne(query);
+    console.log(movie);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
