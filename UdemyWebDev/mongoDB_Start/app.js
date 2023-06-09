f0439a41c7c4894aa1f18ba867b8a0cd672f5c7f
TrevorKvanvig <tkvanvig10@gmail.com>
2023-06-08 20:01:35 -0700

    test MongoDB

--- UdemyWebDev/mongoDB_Start/app.js
+++ UdemyWebDev/mongoDB_Start/app.js
@@ -1,17 +1,33 @@
 const secrets = require(__dirname + "/secrets.js")
+const assert = require('assert')
 
 const { MongoClient } = require("mongodb");
 // Replace the uri string with your connection string.
+
+
 const uri = "mongodb+srv://Cluster91362:"+secrets.getPassword()+"@cluster91362.pghfyon.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri);
 async function run() {
   try {
-    const database = client.db('sample_mflix');
-    const movies = database.collection('movies');
+    const database = client.db('TestDB');
+    const collection = database.collection('Fruits');
     // Query for a movie that has the title 'Back to the Future'
-    const query = { title: 'Back to the Future' };
-    const movie = await movies.findOne(query);
-    console.log(movie);
+    collection.insertMany([
+      {
+        name: "Apple",
+        score: 8,
+        review: "Greate Fruit"
+      },
+      {
+        name: "Apple",
+        score: 8,
+        review: "Greate Fruit"
+      }
+    ], function(err, result){
+      assert.equal(err,null);
+      assert.equal(2, result.result.n);
+      assert.equal(2, result.ops.length)
+    })
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
