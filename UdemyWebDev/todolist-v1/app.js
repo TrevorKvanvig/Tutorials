537d95194b1bb41426f4ee78db03f62330c2b3c1
TrevorKvanvig <tkvanvig10@gmail.com>
2023-06-12 18:09:52 -0700

    Start adding mongoDB to to do list

--- UdemyWebDev/todolist-v1/app.js
+++ UdemyWebDev/todolist-v1/app.js
@@ -1,46 +1,193 @@
+//==================== Requirements ====================
+const { connectToDb, getDb, getDbName } = require(__dirname + "/db.js")
 const express = require("express");
 const bodyParser = require("body-parser");
 const date = require(__dirname + "/date.js")
-
+const ejs = require("ejs");
 const app = express();
+const { ObjectId } = require("mongodb")
 
+//==================== Express setters ====================
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.use(express.static("public"))
 
-const items = ["Buy Food", "Cook Food", "Eat Food"]
-const workItems = []
+//==================== Globals ====================
+//"Buy Food", "Cook Food", "Eat Food"
+// used to store items from database to display in ejs files
+let items = []
+
+// default items if there is no current to do in database
+const defaultItems = [
+    {name: "Welcome to the To Do List"},
+    {name: "Press + to add an item"}, 
+    {name: "Press <- to delete"}
+]
+
+// global variable for databse connected to
+let db
+
+//=========== Functions ===========
+function isValidObjectId(id){
+     
+    if(ObjectId.isValid(id)){
+        if((String)(new ObjectId(id)) === id)
+            return true;       
+        return false;
+    }
+    return false;
+}
+
+//=========== Body ===========
+// SET UP ACCESS
+//establish connection to DB
+connectToDb(function(err){
+  // if no errors while connecting
+  if(!err){
+    //set global database to one connected to form db.js
+    db = getDb()
+    console.log('Successfuly Connected app.js to database on Mongo Atlas Titled: "' + getDbName()+'"');
+
+    // start site and allow listening w=oce database connection is established
+    app.listen(3000, function() {
+      console.log("Server started on port 3000");
+      
+    });
+
+  }else{  
+    // if errors while connecting
+    console.log('app unable to start becasue database not connected');
+  }
+});
+
+app.get("/:customListName", function (req, res) {
+    const custListName = req.params.customListName
+    
+    const list = {
+        name: custListName,
+        items: defaultItems
+    }
+
+    // go into connected database in a collection called toDos
+    db.collection('Lists').find().forEach(item => {
+        // find each item and add it to items array
+        items.push(item)
+    }).then(function(){
+        
+        if(items.length === 0){
+            // if there are no items in the data base render list.ejs with default global items
+            res.status(200).render("list", { listTitle: list.name, newListItems: list.items })
+        }else{
+            // when successful 
+            // render html page with items from database added in for each loop
+            res.status(200).render("list", { listTitle: day, newListItems: items })
+        }
+        
+    }).catch(function(){
+        // else failure
+        console.log("FAILED");
+        res.status(500)
+    })
+    console.log(items);
+
+    // reset items to empty so when site gets reloaded only database memory will keep track of to dos
+    items = []
+
+
+});
+
+
+// AFTER ACCESS IS SET UP
 app.get("/", function (req, res) {
 
+    // get current day from day.js function
     let day = date.getDate()
 
-    res.render("list", { listTitle: day, newListItems: items })
+    // go into connected database in a collection called toDos
+    db.collection('Lists/toDos').find().forEach(item => {
+        // find each item and add it to items array
+        items.push(item)
+    }).then(function(){
+        
+        if(items.length === 0){
+            // if there are no items in the data base render list.ejs with default global items
+            res.status(200).render("list", { listTitle: day, newListItems: defaultItems })
+        }else{
+            // when successful 
+            // render html page with items from database added in for each loop
+            res.status(200).render("list", { listTitle: day, newListItems: items })
+        }
+        
+    }).catch(function(){
+        // else failure
+        console.log("FAILED");
+        res.status(500)
+    })
+    console.log(items);
+
+    // reset items to empty so when site gets reloaded only database memory will keep track of to dos
+    items = []
 
 })
 
 app.post("/", function (req, res) {
-    const newToDo = req.body.newToDo
-
-
-    if (req.body.list === "Work") {
-        workItems.push(newToDo)
-        res.redirect("/work")
-    } else {
-        items.push(newToDo)
-        res.redirect("/")
+    // create json object to add to database
+    const newToDo = {
+        name: req.body.newToDo
     }
+    
+    // go to the todos collection and insert created json object
+    db.collection('Lists/' + req.body.list ).insertOne(newToDo)
+        // then sucellfully added
+        .then(function(result){
+            res.status(201)
+        }).catch(function(){
+            // else did not add
+            console.log('could not add document to ' + getDbName());
+            res.status(500)
+    })
+
+    // redirect back to home page where new item will be loaded becasue it will now be in database
+    if (req.body.list == date.getDate()){
+        res.redirect("/")
+    }else{
+        res.redirect("/" + req.body.list)
+    }
+    
+    
 
 });
 
-app.get("/work", function (req, res) {
-    res.render("list", { listTitle: "Work", newListItems: workItems })
+
+
+app.post("/delete", function (req, res) {
+    idToDelete = req.body.checkbox
+
+    // if Mongo ID is of correct format
+    if(isValidObjectId(idToDelete)){
+
+        // go to todos collection and delete id of checkbox item 
+        db.collection('toDos').deleteOne({_id: new ObjectId(idToDelete)})
+            // once deleted
+            .then(function(result){
+                // redirect to home route with updated database
+                console.log("Sucsessfully Deleted Document with ID ", idToDelete);
+                res.status(200).redirect("/")
+                
+            }).catch(function(){
+                // if faliure
+                console.log("Failed To Delete Item with ID of ", idToDelete);
+                res.status(500)
+            })
+    }else{
+        // if Mongo ID is not correct format
+        res.status(500)
+        console.log("ERROR: Not Valid Doc ID");
+    }
+    
 });
 
-app.get("/about", function (req, res) {
-    res.render("about")
-});
 
 
-app.listen(3000, function () {
-    console.log("listening on 3000");
-})
+
+
---
+++ UdemyWebDev/todolist-v1/db.js
@@ -0,0 +1,45 @@
+const { MongoClient, ObjectId} = require("mongodb")
+const secrets = require(__dirname + '/secrets.js')
+let dbName = 'ToDoList'
+const uri = "mongodb+srv://Cluster91362:"+secrets.getPassword()+"@cluster91362.pghfyon.mongodb.net/"+dbName+"?retryWrites=true&w=majority"
+
+
+let dbConnection
+
+module.exports = {
+  connectToDb: function(callBack){
+    MongoClient.connect(uri)
+    .then(function(client){
+      // set database retruned to variable dbConnection
+      dbConnection = client.db()
+      //retrun a empty callback function signifying success
+      return callBack()
+
+    }).catch(function(err){
+      //display error
+      console.log("Unable to connect to DataBase");
+      //retrun callback function with error message
+      return callBack(err)
+
+    })
+  },
+
+  getDb: function(){
+    
+    // if the database does not exists
+    if(!dbConnection){
+      console.log("No current DB connection");
+
+    }else{
+      // if database exists
+      // give the database that has been connected to back
+      return dbConnection
+    }
+    
+  },
+
+
+  getDbName: function(){
+    return dbName;
+  }
+}
