1ed91499bfd0b070ae51b5980e11a588bc70eac4
TrevorKvanvig <tkvanvig10@gmail.com>
2023-06-13 13:34:49 -0700

    Finish To Do List

--- UdemyWebDev/ejs-challenge/app.js
+++ UdemyWebDev/ejs-challenge/app.js
@@ -1,18 +1,49 @@
+//===========Requirements===========
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const contents = require(__dirname + "/contents.js")
+const { connectToDb, getDb } = require(__dirname + "/db.js")
 const lodash = require('lodash');
 
 
+//===========Set App===========
+// access express functionality through app var
 const app = express();
 
+// allows express to render ejs files
 app.set('view engine', 'ejs');
 
+// allows express to use body parser
 app.use(bodyParser.urlencoded({extended: true}));
+
+// allows express to use css files before loading site in "public folder"
 app.use(express.static("public"));
 
+//===========Globals===========
 const posts = []
+let db
+
+
+//===========Body===========
+//establish connection to DB
+connectToDb(function(err){
+  // if no errors while connecting
+  if(!err){
+    //set global database to one connected to form db.js
+    db = getDb()
+    console.log('Successfuly connected app.js to db');
+
+    // start site and allow listening w=oce database connection is established
+    app.listen(3000, function() {
+      console.log("Server started on port 3000");
+    });
+
+  }else{  
+    // if errors while connecting
+    console.log('app unable to start becasue database not connected');
+  }
+});
 
 
 app.get("/", function(req, res){
@@ -56,17 +87,4 @@ app.post("/compose", function(req, res){
   }
   posts.push(newPost)
   res.redirect("/")
-});
-
-
-
-
-
-
-
-
-
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
 });
---
+++ UdemyWebDev/ejs-challenge/db.js
@@ -0,0 +1,38 @@
+const {MongoClient} = require("mongodb")
+const secrets = require(__dirname + '/secrets.js')
+
+const uri = "mongodb+srv://Cluster91362:"+secrets.getPassword()+"@cluster91362.pghfyon.mongodb.net/ToDoList?retryWrites=true&w=majority"
+let dbConnection
+
+module.exports = {
+  connectToDb: function(callBack){
+    MongoClient.connect(uri)
+    .then(function(client){
+      // set database retruned to variable dbConnection
+      dbConnection = client.db()
+      //retrun a empty callback function signifying success
+      return callBack()
+
+    }).catch(function(err){
+      //display error
+      console.log("Unable to connect to DataBase");
+      //retrun callback function with error message
+      return callBack(err)
+
+    })
+  },
+
+  getDb: function(){
+    
+    // if the database does not exists
+    if(!dbConnection){
+      console.log("No current DB connection");
+
+    }else{
+      // if database exists
+      // give the database that has been connected to back
+      return dbConnection
+    }
+    
+  }
+}
