a2a8c8155c0f71e736bed48314f7226a4c38f365
TrevorKvanvig <tkvanvig10@gmail.com>
2023-06-21 16:04:58 -0700

    Finish React Module

---
+++ UdemyWebDev/React/keeper-app-with-extra-styling/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import FolderSpecialIcon from "@mui/icons-material/FolderSpecial";
+function Header() {
+  return (
+    <header>
+      <h1>
+        <FolderSpecialIcon />
+        Keeper
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
