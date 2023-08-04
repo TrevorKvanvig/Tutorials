3950728da5363c4c3a64cb42b4800c26e9b41b35
TrevorKvanvig <tkvanvig10@gmail.com>
2023-08-04 10:49:36 -0700

    React Router Practice

---
+++ React-Router/frontend/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+import {Routes, Route, Link} from 'react-router-dom'
+import Home from './pages/Home';
+import Books from './pages/Books';
+
+function App() {
+  return (
+    <>
+    <nav>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/books">Books</Link></li>
+      </ul>
+    </nav>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/books" element={<Books/>}/>
+      
+    </Routes>  
+    </>
+    
+  );
+}
+
+export default App;
