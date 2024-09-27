cd44b2593c3be9227d3de38e2cc717a764eaa99c
TrevorKvanvig <tkvanvig10@gmail.com>
2024-09-26 21:02:19 -0700

    Models

--- Dotnet-Course-Code/HelloWorld/Program.cs
+++ Dotnet-Course-Code/HelloWorld/Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
+using HelloWorld.Models;
 
 namespace HelloWorld
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        static void Main(string[] args){
+          Computer myComputer = new Computer(){
+            Motherboard = "Z690",
+            HasWifi = true,
+            HasLTE = false,
+            ReleaseDate = DateTime.Now,
+            Price = 943.87m,
+            VideoCard = "RTX 4080"
+
+          };
+
+          Console.WriteLine(myComputer.Motherboard);
         }
     }
 }
