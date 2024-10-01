cc2f9c3e2cb279d2f91c913ee24ff59f99c30486
TrevorKvanvig <tkvanvig10@gmail.com>
2024-09-30 18:19:37 -0700

    SQL Statements

--- Dotnet-Course-Code/HelloWorld/Program.cs
+++ Dotnet-Course-Code/HelloWorld/Program.cs
@@ -1,22 +1,72 @@
 ﻿using System;
+using System.Data;
+using Dapper;
 using HelloWorld.Models;
+using Microsoft.Data.SqlClient;
 
 namespace HelloWorld
 {
     internal class Program
     {
-        static void Main(string[] args){
-          Computer myComputer = new Computer(){
-            Motherboard = "Z690",
-            HasWifi = true,
-            HasLTE = false,
-            ReleaseDate = DateTime.Now,
-            Price = 943.87m,
-            VideoCard = "RTX 4080"
+      static void Main(string[] args){
+        string connectionString = "Server=localhost;Database=DotNetCourseDatabase;TrustServerCertificate=true;Trusted_Connection=false;User ID=SA;Password=D1mcBWHP";
 
-          };
+        IDbConnection dbConnection = new SqlConnection(connectionString);
 
-          Console.WriteLine(myComputer.Motherboard);
+        string sqlCommand = "SELECT GETDATE()";
+
+        DateTime RightNow = dbConnection.QuerySingle<DateTime>(sqlCommand);
+
+        Computer myComputer = new Computer(){
+          Motherboard= "Z90df4",
+          CPUCores = 8,
+          HasWifi = true,
+          HasLTE = false,
+          ReleaseDate = DateTime.Now,
+          Price = 12.56m,
+          VideoCard = "RTX 3090"
+        };
+
+        string sql = @"INSERT INTO TutorialAppSchema.Computer (
+          Motherboard,
+          CPUCores,
+          HasWifi,
+          HasLTE,
+          ReleaseDate,
+          Price,
+          VideoCard
+        ) VALUES ('" + myComputer.Motherboard 
+            + "','" + myComputer.CPUCores
+            + "','" + myComputer.HasWifi
+            + "','" + myComputer.HasLTE
+            + "','" + myComputer.ReleaseDate.ToString("yyyy-MM-dd")
+            + "','" + myComputer.Price
+            + "','" + myComputer.VideoCard 
+        + "')";
+
+        string sqlSelect = @"Select 
+          Computer.Motherboard, 
+          Computer.CPUCores, 
+          Computer.HasWifi, 
+          Computer.HasLTE, 
+          Computer.ReleaseDate, 
+          Computer.Price, 
+          Computer.VideoCard
+        FROM TutorialAppSchema.Computer";
+
+        IEnumerable<Computer> computers = dbConnection.Query<Computer>(sqlSelect);
+
+        foreach (Computer singleComputer in computers){
+          Console.WriteLine("'" + myComputer.Motherboard 
+            + "','" + myComputer.CPUCores
+            + "','" + myComputer.HasWifi
+            + "','" + myComputer.HasLTE
+            + "','" + myComputer.ReleaseDate.ToString("yyyy-MM-dd")
+            + "','" + myComputer.Price
+            + "','" + myComputer.VideoCard 
+        + "')");
         }
+        Console.WriteLine(sqlSelect); 
+      }
     }
 }
--- Dotnet-Course-Code/HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ Dotnet-Course-Code/HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e73bde77e6c023d1aa1143bcdd9ed1e21fd953b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd44b2593c3be9227d3de38e2cc717a764eaa99c")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
