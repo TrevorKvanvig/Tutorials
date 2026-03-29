c2bd08e060bb0934c2d087a569170576ed17f392
TrevorKvanvig <tkvanvig10@gmail.com>
2026-03-29 15:45:20 -0700

    Add react-movies project

--- react-movies/src/components/Loading.tsx
+++
@@ -1,4 +0,0 @@
-import loadingImg from "../assets/standby-loading.gif"
-export default function Loading() {
-    return <img src={loadingImg} alt="Loading...."/>
-}
--- react-movies/src/features/movies/models/movie.model.tsx
+++
@@ -1,5 +0,0 @@
-export default interface Movie {
-    id: number,
-    title: string,
-    poster: string
-}
