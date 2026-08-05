a27809a916ddd571466bfb077bb4f5a464a19bce
TrevorKvanvig <tkvanvig10@gmail.com>
2026-08-04 18:07:29 -0700

    Create React App

--- Finshark/api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ Finshark/api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e014703e60c004f515c5c90d7b29d356095b4ad8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2bd08e060bb0934c2d087a569170576ed17f392")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
